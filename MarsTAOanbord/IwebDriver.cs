e2a3fe2296826ed8393c8f88d3285f910a6ebd26
Antony Thomas <antonythomas03@hotmail.com>
2022-10-08 13:54:18 +1300

    feat: setup project structure

---
+++ MarsTAOanbord/IwebDriver.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace MarsTAOanbord
+{
+    internal class IwebDriver
+    {
+        public static implicit operator IwebDriver(ChromeDriver v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Manage()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Navigate()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IWebElement FindElement(By by)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MarsTAOanbord/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.Services.Common;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace MarsTAOanbord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // open chrome browser
+           IWebDriver driver = new ChromeDriver();
+
+            //lunch turnup portal
+            driver.Navigate().GoToUrl("http://localhost:5000/Home");
+
+            // identify emailID textbox and enter valid emailID
+            IWebElement emailTextbox = driver.FindElement(By.Name("email"));
+            emailTextbox.SendKeys("antonythomas03@hotmail.com");
+
+            // identify password textbox and enter valid password
+            IWebElement passwordTextbox = driver.FindElement(By.Name("password"));
+            passwordTextbox.SendKeys("IcanDo!t1");
+
+            // click on login button
+            IWebElement loginButton = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[4]/button"));
+            loginButton.Click();
+
+            // check if user is logged in successfully
+            IWebElement helloAntony = driver.FindElement(By.XPath("//*[@id='account - profile - section']/div/div[1]/div[2]/div/a[2]/button"));
+
+            if(helloAntony.Text == "Hello antony!")
+            {
+                Console.WriteLine("Logged in successfully, test passed.");
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
